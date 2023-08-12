4142806733c12d19c441af44cac2d7c0780b67af
21a31a0453nagababu <126693796+21a31a0453nagababu@users.noreply.github.com>
2023-08-12 17:50:08 +0530

    Add files via upload

---
+++ task1.js
@@ -0,0 +1,26 @@
+function getWeather() {
+    const apiKey = '988b35ab231c3c2dab2239bfb456a319';
+    const city = document.getElementById('cityInput').value;
+
+    if (!city) {
+        alert('Please enter a city');
+        return;
+    }
+
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            const weatherInfo = document.getElementById('weatherInfo');
+            weatherInfo.innerHTML = `
+                <h2>Weather in ${data.name}</h2>
+                <p>Temperature: ${data.main.temp}°C</p>
+                <p>Weather: ${data.weather[0].description}</p>
+                <p>Wind Speed: ${data.wind.speed} m/s</p>
+            `;
+        })
+        .catch(error => {
+            console.error('Error fetching weather data:', error);
+        });
+}
